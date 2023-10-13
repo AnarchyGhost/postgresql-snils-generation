773e165f62d750dc414b8edc426441d1421d1355
k.markelov <k.markelov@energochain.ru>
2023-10-13 10:24:15 +0300

    Initial commit

---
+++ generate_snils.sql
@@ -0,0 +1,24 @@
+create or replace function generate_snils() returns text
+    language plpgsql
+as
+$$
+declare
+digits int[];
+    multiplier int[] not null default array[9,8,7,6,5,4,3,2,1];
+sum int not null default 0;
+    result text;
+    i int default 1;
+begin
+    result := CAST(100000000 + floor(random() * 899999999) AS bigint)::text;
+    digits := string_to_array(result, NULL)::int[];
+sum:=0;
+    i := 1;
+    while multiplier[i] is not null
+        loop
+            sum := sum + multiplier[i] * digits[i];
+            i := i + 1;
+end loop;
+    result := concat(result,(sum % 101 % 100)::int);
+return result;
+end;
+$$;
